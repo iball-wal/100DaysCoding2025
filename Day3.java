47acd2eff446798e1cec3550c28ae28d304396ea
iball-wal <muhammadiqwal126@gmail.com>
2025-09-10 23:18:58 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class Day3 {
+    public static void main(String[] args) {
+
+        // single line koment
+        System.out.println("ini adalah contoh komentar menggunakan koment line (// halo nama say iqwal))");
+        // sinngle line koment ini biasanya digunakan hanya untuk memberikan keterangan pada 1 baris saja //
+        System.out.println("ini adalah contoh penggunaan multi line comnet (/* hallo juga nama saya rohani*/)");
+        /* muli line commnet ini digunakan untuk komentar yang lebih panjang,
+         bisa mencakup lebih dari 1 baris*/
+        System.out.print("ini contoh penggunakan documentation commnet (/** haiii */)");
+    }
+}
