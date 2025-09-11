505edf22cc24dc8f7d1f8b5b184e9f9c450c321e
iball-wal <muhammadiqwal126@gmail.com>
2025-09-11 20:55:48 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,18 @@
+public class Day4 {
+    public static void main(String[] args) {
+        //1. new Line
+        System.out.print("1. hallo\nabang\n");
+        //2. Tab
+        System.out.println("2. nama\tsaya\tiqwal");
+        //3. Single quete
+        System.out.println("3. It\'s me time");
+        //4. double quote
+        System.out.println("4. dia bilang \"aku suka kamu\"");
+        //5. Backslash
+        System.out.println("5. Path: C:\\User\\iqwal");
+        //6.Backspace
+        System.out.println("6. ini java\b!");
+        //7.Carriage return
+        System.out.println(" Hellow\r7. Word");
+    }
+}
