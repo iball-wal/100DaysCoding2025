454e6a4a5ecca15839d7f430428d9cdaa0d22871
iball-wal <muhammadiqwal126@gmail.com>
2025-09-12 18:27:51 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class Day5 {
+    public static void main(String[] args) {
+        // mencetak dengan 4 tipe data bilangan bulat
+      
+        // 1.byte
+        byte digit = 10;
+        System.out.println("nilai byte : " + digit);
+        // 2. short
+        short value  = -12;
+        System.out.println("nilai short : " + value);
+        // 3. Integer
+        int age = 19;
+        System.out.println("nilai int : " + age);
+        // 4. long
+        long time = 1225;
+        System.out.println("nilai long time : " + time);
+    }
+}
