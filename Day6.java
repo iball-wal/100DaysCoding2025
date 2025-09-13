d11a6732194983e7adecfb009d5097de71a5cdf4
iball-wal <muhammadiqwal126@gmail.com>
2025-09-13 18:24:57 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+public class Day6 {
+    public static void main(String[] args) {
+        // variable
+        float xF = 12.6f;
+        double xD = 12.6245535;
+
+        // cetak
+        System.out.println("Nilai float : " + xF);
+        System.out.println("Nilai double : " + xD);
+    }
+}
