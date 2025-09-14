3caa4753017a2ebfb76550ec3117e4ff499df2e0
iball-wal <muhammadiqwal126@gmail.com>
2025-09-14 22:57:49 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,18 @@
+public class Day7 {
+    public static void main(String[] args) {
+        // char
+        char huān = '欢';
+        char yíng = '迎';
+        char guāng = '光';
+        char lín = '临';
+
+        System.out.print(huān);
+        System.out.print(yíng);
+        System.out.print(guāng);
+        System.out.println(lín);
+
+        //boolean
+        boolean hujan = true;
+        System.out.println("Apakah malam ini hujan : " + hujan);
+    }
+}
