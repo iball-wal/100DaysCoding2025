f3e23fd558509785e9afe32d7a02484688d81f57
iball-wal <muhammadiqwal126@gmail.com>
2025-09-15 20:08:56 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class Day8 {
+    public static void main(String[] args) {
+        String alasan = "alasan saya masuk ISC ingin mencari rival dalam dunia percodingan saat saya telah belajar banyak hal ";
+        String pilihan_informatika = "alasan mengambil informatika ini, karena saya merasa membuat lebih baik dari pada sekedar menggunakan ";
+        String salah_pilih = "tentunya tidak, justru saya butuh pressure yang lebih strong,ya walaupun kadang tembus pagi but " +
+                "PRESURRENYA MASIH KURANGGGGGGGGGG";
+        String kendala = "kendala saya sejauh ini hanya berada pada awal amsuk perkulihan, karena pada saat itu sayakan orang baru di majene " +
+                "jadi masih urang nyaman pada watku itu di perantauan. hal itu lah yan gmenghambat pekrkembangan saya mulai dari akademik dan non akademik";
+        String tambahan = "Tapi sekrang Kasih keras KINGGGG";
+
+        System.out.println("Alasan masuk ISC : " +alasan);
+        System.out.println("Kenapa pilih Informatika : " +pilihan_informatika);
+        System.out.println("Salah pilih atau tidak : " +salah_pilih);
+        System.out.println("Apa kendala DDP/PBO : " + kendala);
+        System.out.println("Tambahannya bang : " + tambahan);
+    }
+}
