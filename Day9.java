04be90eea9e9bb9332f24e25d40608ce9143530d
iball-wal <muhammadiqwal126@gmail.com>
2025-09-16 21:07:30 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,22 @@
+public class Day9 {
+    public static void main(String[] args) {
+
+        // deklarasi
+        String nama;
+        String prodi;
+        String Nim;
+        char kelas;
+
+        // Inisialisasi
+        nama = "muhammad iqwal";
+        prodi = "Teknik Informatika";
+        Nim = "D0224025";
+        kelas = 'C';
+
+        System.out.println("nama mahasiswa : " + nama);
+        System.out.println("Prodi pilihan : " + prodi);
+        System.out.println("Nim Mahasiswa : " + Nim);
+        System.out.println("Kelas  : " + kelas);
+
+    }
+}
