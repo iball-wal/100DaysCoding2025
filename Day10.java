a962cd90ff8c8ea746307c695c11e0d6ffbfe825
iball-wal <muhammadiqwal126@gmail.com>
2025-09-17 20:46:13 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // jumlah hari dalam 1 minggu = 7 (konstanta)
+        final int Hari_Dalam_seminggu = 7;
+        int jumlah_minggu = 4;
+        int totalHari = Hari_Dalam_seminggu * jumlah_minggu;
+        System.out.println("Jumlahm minggu : " + jumlah_minggu);
+        System.out.println("Total hari : " + totalHari);
+    }
+}
