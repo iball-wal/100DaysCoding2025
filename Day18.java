7af74c397672cdbdb450dbb6d87e152cabb4e42d
iball-wal <muhammadiqwal126@gmail.com>
2025-09-25 20:29:49 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,10 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int hargaBarang =150000;
+        double pajak = 16500.50;
+
+        double totalBayar = hargaBarang + pajak;
+        System.out.println("totoal bayar : " + totalBayar);
+
+    }
+}
