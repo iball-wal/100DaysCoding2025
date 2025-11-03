44b1c18d8df1b15749005c68432c703291140209
iball-wal <muhammadiqwal126@gmail.com>
2025-11-03 22:13:01 +0800

    Day57.java

---
+++ Day57.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day56 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("masukkan ukuran sisi persegi (N) :");
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || i == n - 1 || j == 0 || j == n-1 ){
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println("");
+        }
+    }
+}
