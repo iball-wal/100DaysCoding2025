357b8d517d7f6013c40d5eba85e6bb040524d468
iball-wal <muhammadiqwal126@gmail.com>
2025-11-14 22:51:41 +0800

    Day68,java

---
+++ Day68.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day68 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan panajng persigi : ");
+        int n =  in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+}
