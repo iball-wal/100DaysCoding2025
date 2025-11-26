121418265ebeff7b83dbc1401d3d66fe4b42a450
iball-wal <muhammadiqwal126@gmail.com>
2025-11-26 23:46:13 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,8 @@
+public class Day80 {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        for(int b : a){
+            System.out.println(b);
+        }
+    }
+}
