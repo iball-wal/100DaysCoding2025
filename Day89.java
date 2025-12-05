015780146984c40ef900dbc32b95ed9413f6fe73
iball-wal <muhammadiqwal126@gmail.com>
2025-12-05 22:52:33 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,9 @@
+public class Day89 {
+    public static void main(String[] args) {
+        Day89 day89 = new Day89();
+        day89.sapaDefault();
+    }
+    void sapaDefault() {
+        System.out.println("Halo, selamat belajar java!");
+    }
+}
