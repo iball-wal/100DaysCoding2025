832648fe2fa7c5d39b5f71c823656fda95da2064
iball-wal <muhammadiqwal126@gmail.com>
2025-12-09 20:17:44 +0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,13 @@
+public class Day93 {
+    public static void main(String[] args) {
+        // bulatkan ke atas
+        double x =  Math.ceil(12.3);
+        System.out.println(x);
+        // bulatkan ke bawah
+        double y= Math.floor(12.4);
+        System.out.println(y);
+        // bulatkan terdekat > / <
+        double z = Math.round(12.5);
+        System.out.println(z);
+    }
+}
